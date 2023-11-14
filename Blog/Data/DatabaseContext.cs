658acad142df01bae9ce959a4f476a4b780fc1c9
LightNur <150859759+LightNur@users.noreply.github.com>
2023-11-14 16:53:31 -0500

    Add files via upload

---
+++ Blog/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Models.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Post> Post { get; set; }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Models
+{
+	public class Post
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string LongDesc { get; set; }
+		public string ShortDesc { get; set; }
+		public string Author { get; set; }
+		public DateTime Created { get; set; }
+		
+	}
+}
